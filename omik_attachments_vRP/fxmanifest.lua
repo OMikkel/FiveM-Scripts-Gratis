3439d0be1b7bf7c77cf8d9191f838f806f3a777c
OMikkel <Mikkelmaae@gmail.com>
2020-12-16 15:39:53 +0100

    Added vRP

--- omik_attachments_vRP/fxmanifest.lua
+++ omik_attachments_vRP/fxmanifest.lua
@@ -10,11 +10,15 @@ ui_page "html/index.html"
 
 client_scripts {
     "config.lua",
+    "lib/Tunnel.lua",
+    "lib/Proxy.lua",
     "client.lua"
 }
 
 server_scripts {
     "config.lua",
+    "@mysql-async/lib/MySQL.lua",
+    "@vrp/lib/utils.lua",
     "server.lua"
 }
 
